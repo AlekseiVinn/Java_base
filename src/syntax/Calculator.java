ccac5a13566a5ff91c3004d4d2980f847edbc753
Alexei Vinnikov <eternal.creature.vin@gmail.com>
2024-04-16 17:28:39 +0300

    task 2: syntax

---
+++ src/syntax/Calculator.java
@@ -0,0 +1,54 @@
+package syntax;
+
+public class Calculator {
+    //int
+    public static int Summ(int num1, int num2){
+        return (num1 + num2);
+    }
+
+    public static int Substr(int num1, int num2){
+        return (num1 - num2);
+    }
+
+    public static int Div(int num1, int num2){
+        return (num1 / num2);
+    }
+
+    public static int Multiply(int num1, int num2){
+        return (num1 * num2);
+    }
+
+    //double
+    public static double Summ(double num1, double num2){
+        return (num1 + num2);
+    }
+
+    public static double Substr(double num1, double num2){
+        return (num1 - num2);
+    }
+
+    public static double Div(double num1, double num2){
+        return (num1 / num2);
+    }
+
+    public static double Multiply(double num1, double num2){
+        return (num1 * num2);
+    }
+
+    //long
+    public static long Summ(long num1, long num2){
+        return (num1 + num2);
+    }
+
+    public static long Substr(long num1, long num2){
+        return (num1 - num2);
+    }
+
+    public static long Div(long num1, long num2){
+        return (num1 / num2);
+    }
+
+    public static long Multiply(long num1, long num2){
+        return (num1 * num2);
+    }
+}
